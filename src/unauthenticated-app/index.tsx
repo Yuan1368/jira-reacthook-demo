d4328fbe1776674f9ecee5740eaa5ac81e9bb270
bearbaba <2200522850@qq.com>
2021-12-11 23:36:22 +0800

    chore: 添加 emotion 更改页面样式

--- src/unauthenticated-app/index.tsx
+++ src/unauthenticated-app/index.tsx
@@ -1,7 +1,7 @@
 import { LoginPage } from "./login";
 import { RegisterPage } from "./register";
 import React, { useState } from "react";
-import { Card, Divider } from "antd";
+import { Button, Card, Divider } from "antd";
 import styled from "@emotion/styled";
 import logo from "assets/logo.svg";
 import right from "assets/right.svg";
@@ -13,7 +13,8 @@ export const UnauthenticatedApp = () => {
   return (
     <Container>
       <Headers></Headers>
-
+      <Background />
+      <Title>{isRegister ? "请注册" : "请登录"}</Title>
       <ShadowCard>
         {isRegister ? <RegisterPage /> : <LoginPage />}
         <Divider />
@@ -25,11 +26,21 @@ export const UnauthenticatedApp = () => {
   );
 };
 
+export const LongButton = styled(Button)`
+  width: 100%;
+`;
+
+const Title = styled.h2`
+  margin-bottom: 2.4rem;
+  color: rgb(94, 108, 132);
+`;
+
 const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   min-height: 100vh;
+  position: relative;
 `;
 
 const ShadowCard = styled(Card)`
@@ -40,6 +51,18 @@ const ShadowCard = styled(Card)`
   box-sizing: border-box;
   box-shadow: rgba(0, 0, 0, 0.1) 0 0 10px;
   text-align: center;
+`;
+
+const Background = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-position: left bottom, right bottom;
+  background-size: calc(((100vw - 40rem) / 2) - 3.2rem),
+    calc(((100vw - 40rem) / 2) - 3.2rem), cover;
+  background-image: url(${left}), url(${right});
 `;
 
 const Headers = styled.header`
